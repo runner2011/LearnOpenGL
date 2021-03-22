fb00123aa2017eaa851375d655fcafdc0679756c
Runner2011 <chenjfsea@gmail.com>
2021-03-22 23:11:08 +0800

    exercise: offset vertices

--- Source/main.cpp
+++ Source/main.cpp
@@ -110,7 +110,7 @@ int main()
 
 		// draw our first triangle
 		ourShader.use();
-		//glUseProgram(shaderProgram);
+		ourShader.setFloat3("offset", 0.5f, 0, 0);
 
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 

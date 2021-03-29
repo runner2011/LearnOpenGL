d65ef8b2b3863b350c0398c85105a109b1de1902
Runner2011 <chenjfsea@gmail.com>
2021-03-29 22:30:53 +0800

    exercise 2: repeat 4 tex2

--- Source/FragmentShaderFile.fs
+++ Source/FragmentShaderFile.fs
@@ -10,5 +10,5 @@ uniform sampler2D texture2;
 void main()
 {
     //FragColor =  mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
-    FragColor =  texture(texture1, TexCoord) * 0.8 + texture(texture2, vec2(-TexCoord.x, TexCoord.y)) * 0.2;
+    FragColor =  texture(texture1, TexCoord * 2) * 0.8 + texture(texture2, TexCoord * 2) * 0.2;
 }

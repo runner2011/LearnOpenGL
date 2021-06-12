da0f2fd64e4899c2f671b1413dab5d16297df129
Runner2011 <chenjfsea@gmail.com>
2021-06-12 14:36:11 +0800

    Add camera

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,11 +6,16 @@
 #include <fstream>
 #include <LearnOpenGL/Shader_s.h>
 #include <LearnOpenGL/stb_image.h>
+#include <LearnOpenGL/camera.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
 
 // link shaders
@@ -25,6 +30,16 @@ float mix_tex1 = 0.8f;
 int windowWidth = 1100;
 int windowHeight = 880;
 
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = windowWidth / 2.0f;
+float lastY = windowHeight / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -37,7 +52,44 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if(firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
 
 void SetupTexture(const char* path, unsigned int& referenceID, GLint internalFormat, GLenum format)
 {
@@ -174,8 +226,14 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -193,13 +251,19 @@ int main()
 	SetupApplicationData();
 
 	// light position
-	//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-	glm::vec3 lightPos(-1.f, 1.0f, -6.f);
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+	//glm::vec3 lightPos(-1.f, 1.0f, -6.f);
 	
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// per-frame time logic
+		// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//input
 		processInput(window);
 
@@ -222,11 +286,11 @@ int main()
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::rotate(model, glm::radians(60.f), glm::vec3(0.5f, 1.0f, 0.0f));
 		//view
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+		glm::mat4 view = camera.GetViewMatrix();
+		//view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
 		//projection
 		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.f), (float)windowWidth / windowHeight, 0.1f, 100.f);
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)windowWidth / windowHeight, 0.1f, 100.f);
 
 		lightingShader.setMatrix4("model", glm::value_ptr(model));
 		lightingShader.setMatrix4("view", glm::value_ptr(view));
@@ -240,7 +304,7 @@ int main()
 		lightingShader.setFloat3("lightColor", lightR, lightG, lightB);
 		lightingShader.setVec3("lightPos", lightPos);
 
-		// draw our first triangle
+		/// DRAW
 		glBindVertexArray(lightingVAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
@@ -250,12 +314,12 @@ int main()
 		//model
 		model = glm::mat4(1.0f);
 		model = glm::rotate(model, glm::radians(60.f), glm::vec3(0.5f, 1.0f, 0.0f));
-		//model = glm::translate(model, lightPos);
+		model = glm::translate(model, lightPos);
 		
 		//view
 		// TODO DELETE
-		view = glm::mat4(1.0f);
-		view = glm::translate(view, lightPos);
+		//view = camera.GetViewMatrix();
+		//view = glm::translate(view, lightPos);
 
 		lightShader.setMatrix4("model", glm::value_ptr(model));
 		lightShader.setMatrix4("view", glm::value_ptr(view));
@@ -263,6 +327,8 @@ int main()
 
 		lightShader.setFloat3("lightColor", lightR, lightG, lightB);
 
+		
+		/// DRAW
 		glBindVertexArray(lightVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 

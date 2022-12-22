fc2868032c604a7d342ba0134b26cf82371a1f12
runner2011 <chenjfsea@gmail.com>
2022-12-22 23:29:50 +0800

    Load model, first try. Failed show textures.

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <LearnOpenGL/Shader_s.h>
-#include <LearnOpenGL/stb_image.h>
+#include <LearnOpenGL/Model.h>
 #include <LearnOpenGL/camera.h>
 
 #include <glm/glm.hpp>
@@ -21,14 +21,12 @@ void processInput(GLFWwindow* window);
 // link shaders
 unsigned int shaderProgram;
 
-unsigned int lightingVBO, lightingVAO, lightVAO;
 
-unsigned int texture1, texture2;
 
 float mix_tex1 = 0.8f;
 
-int windowWidth = 1100;
-int windowHeight = 880;
+int windowWidth = 1600;
+int windowHeight = 1200;
 
 // camera
 Camera camera(glm::vec3(0.0f, 1.0f, 3.0f));
@@ -121,83 +119,13 @@ void SetupTexture(const char* path, unsigned int& referenceID, GLint internalFor
 	stbi_image_free(data);
 }
 
-void SetupApplicationData()
+Model SetupApplicationData()
 {
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	// load models
+	// -----------
+	Model model1("../Res/backpack/backpack.obj");
+	return model1;
 
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-    };
-
-	glGenVertexArrays(1, &lightingVAO);
-	glGenBuffers(1, &lightingVBO);
-
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	/***
-	 *** Any subsequent VBO, EBO, calls will be stored inside the VAO currently bound .
-	 ***/
-	glBindVertexArray(lightingVAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, lightingVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// position
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// normal
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	// The second VAO is for next chapter. Right we could no need this.
-	glGenVertexArrays(1, &lightVAO);
-	glBindVertexArray(lightVAO);
-
-	// we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
-	glBindBuffer(GL_ARRAY_BUFFER, lightingVBO);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 
 	//wireframe mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -252,10 +180,11 @@ int main()
 
 	Shader lightingShader("../Source/lighting.vs", "../Source/lighting.fs");
 	Shader lightShader("../Source/light.vs", "../Source/light.fs");
-	SetupApplicationData();
+	Model model1 = SetupApplicationData();
 
 	// light position
-	glm::vec3 lightPos(-1.2f, 2.5f, -6.0f);
+	float a = 100;
+	glm::vec3 lightPos(-1.2f* a, 2.5f* a, 0/*-6.0f*a*/);
 	
 
 	// render loop
@@ -287,7 +216,7 @@ int main()
 		
 		//model
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians(30.f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(0.f), glm::vec3(0.0f, 1.0f, 0.0f));
 		//view
 		glm::mat4 view = camera.GetViewMatrix();
 		//view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
@@ -303,33 +232,12 @@ int main()
 		float lightG = 1.f;
 		float lightB = 1.f;
 
-		lightingShader.setFloat3("objectColor", 1.f, 0.5f, 0.31f);
+		lightingShader.setFloat3("objectColor", 1.f, 1.f, 1.f);
 		lightingShader.setFloat3("lightColor", lightR, lightG, lightB);
 		lightingShader.setVec3("lightPos", lightPos);
 		lightingShader.setVec3("viewPos", camera.Position);
 
-		/// DRAW
-		glBindVertexArray(lightingVAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
-		lightShader.use();
-		
-		//model
-		model = glm::mat4(1.0f);
-		//model = glm::rotate(model, glm::radians(60.f), glm::vec3(0.5f, 1.0f, 0.0f));
-		model = glm::translate(model, lightPos);
-
-		lightShader.setMatrix4("model", glm::value_ptr(model));
-		lightShader.setMatrix4("view", glm::value_ptr(view));
-		lightShader.setMatrix4("projection", glm::value_ptr(projection));
-
-		lightShader.setFloat3("lightColor", lightR, lightG, lightB);
-
-		
-		/// DRAW
-		glBindVertexArray(lightVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model1.Draw(lightingShader);
 
 
 		// check and call events and swap the buffers

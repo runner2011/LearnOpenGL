1ce0459f32d5c8f48ce1d627465944f8f96ba806
runner2011 <chenjfsea@gmail.com>
2026-07-06 17:00:15 +0800

    pick [build]add cross-platform cmake build

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
 #include <iostream>
 #include <fstream>
 #include <LearnOpenGL/Shader_s.h>
@@ -307,7 +308,7 @@ int main(int argc, char** argv)
 		lastFrameTime = currentFrame;
 
 		char title[256];
-		sprintf_s(title, "FPS: %.2f", fps);
+		std::snprintf(title, sizeof(title), "FPS: %.2f", fps);
 		glfwSetWindowTitle(window, title);
 
 		//input
@@ -359,6 +360,3 @@ int main(int argc, char** argv)
 
 void Init(std::string ShaderPath)
 {
-
-}
-
